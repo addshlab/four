d68a779782bfcd0cb4e54f401e89d4b7bb292155
whois <example@example.com>
2020-05-26 00:46:14 +0900

    git init

---
+++ functions.php
@@ -0,0 +1,181 @@
+<?php
+if ( ! isset( $content_width ) ) {
+    $content_width = 600;
+}
+
+/**
+ * テーマにコンポーネントされていないファイルの検知
+ */
+function no_component_files() {
+    global $pagenow;
+    if ( $pagenow !== 'index.php' ) return;
+
+    $message = '<p>fourテーマのリリースビルドに不要なファイルが含まれています。</p>';
+    $flag = 0;
+    $theme_dir_path = get_stylesheet_directory();
+    $allow_files = array(
+        $theme_dir_path . '/functions.php',
+        $theme_dir_path . '/index.php',
+        $theme_dir_path . '/screenshot.png',
+        $theme_dir_path . '/style.css',
+    );
+
+    foreach ( glob( $theme_dir_path . '/*', GLOB_BRACE ) as $file_path ) {
+        if( is_dir( $file_path ) ){
+            if ( false === in_array( $file_path, $allow_files ) ) {
+                $dir_alert .= '<p>テーマディレクトリ内にコンポーネント外のディレクトリ ' . $file_path . ' が存在します。</p>';
+                $flag = 1;
+            }
+        }
+        if( is_file( $file_path ) ){
+            if ( false === in_array( $file_path, $allow_files ) ) {
+                $file_alert .= '<p>テーマディレクトリ内にコンポーネント外のファイル ' . $file_path . ' が存在します。</p>';
+                $flag = 1;
+            }
+        }
+    }
+
+    if ( $flag === 1 ) {
+        echo '<div class="message error">';
+        echo $message;
+        echo $dir_alert;
+        echo $file_alert;
+        echo '</div>';
+    }
+}
+add_action ( 'admin_notices', 'no_component_files' );
+
+/**
+ * 管理画面以外でもDashiconsを使用する
+ * DashiconsはWordPress3.8以上で使用可能なアイコンフォントセットです
+ * @see https://developer.wordpress.org/resource/dashicons/
+ */
+function enqueue_scripts() {
+    wp_enqueue_style( 'dashicons', site_url( '/' ) . '/wp-includes/css/dashicons.min.css' );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
+
+/**
+ * style.cssにファイルタイムスタンプ由来のフィンガープリントを付与
+ * @see https://worklog.be/archives/2983
+*/
+function default_style_fingerprint( $styles ) {
+    $mtime = filemtime( get_stylesheet_directory() . '/style.css' );
+    $styles->default_version = $mtime;
+}
+add_action( 'wp_default_styles', 'default_style_fingerprint' );
+
+/**
+ * テーマのサポートする機能の定義
+ */
+function custom_theme_setup() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'custom-logo' );
+}
+add_action( 'after_setup_theme', 'custom_theme_setup' );
+
+/**
+ * タイトルタグのカスタマイズ
+ */
+function wpdocs_hack_wp_title_for_home( $title ) {
+    if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
+        $title = get_bloginfo( 'name' );
+    } elseif ( is_single() ) {
+        $title = get_the_title() . ' | ' . get_bloginfo( 'name' );
+    } else {
+        $title = get_the_title();
+    }
+    return $title;
+}
+add_filter( 'pre_get_document_title', 'wpdocs_hack_wp_title_for_home' );
+
+/**
+ * Meta Description のカスタマイズ
+ */
+ function add_meta_description() {
+     global $post;
+
+     if ( is_home() ) {
+        $content = get_bloginfo( 'description' );
+    } elseif ( is_singular() ) {
+        if ( $post->post_except ) {
+            $content = esc_attr( $post->post_excerpt );
+        } else {
+            $content = esc_attr( strip_tags( $post->post_content ) );
+            $content = preg_replace( "/[\r\n]/", " ", $content );
+             $content = mb_strimwidth( $content, 0, 100, "..." );    
+        }
+    } else {
+        return;
+    }
+
+     echo '<meta name="description" content="' . $content . '">';
+ }
+add_filter( 'wp_head', 'add_meta_description' );
+
+/**
+ * アーカイブタイトルの整形
+ */
+add_filter( 'get_the_archive_title', function ( $archive_title ) {    
+global $wp_locale;
+    if ( is_category() ) {    
+        $archive_title = single_cat_title( '', false );    
+        } elseif ( is_tag() ) {    
+            $archive_title = single_tag_title( '', false );    
+        } elseif ( is_month() ) {
+            $archive_title = get_query_var( 'year' ) . '年' .get_query_var( 'monthnum' ) . '月';    
+        } elseif ( is_year() ) {
+            $archive_title = get_query_var( 'year' ) . '年';    
+        } elseif ( is_author() ) {    
+            $archive_title = '<span class="vcard">' . get_the_author() . '</span>' ;    
+        } elseif ( is_tax() ) { //for custom post types
+            $archive_title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+        } elseif (is_post_type_archive()) {
+            $archive_title = post_type_archive_title( '', false );
+        }
+    return $archive_title;    
+} );
+
+/**
+ * メニューの定義
+ */
+function menu_init() {
+    $menus = array(
+        'header' => 'Header Menu',
+        'footer' => 'Footer Menu',
+    );
+    register_nav_menus( $menus );
+}
+add_action( 'after_setup_theme', 'menu_init' );
+
+/**
+ * ウィジェットエリアの定義
+ */
+function theme_slug_widgets_init() {
+    register_sidebar(
+        array(
+            'name' => __( 'Sidebar menu', 'four' ),
+            'id' => 'sidebar-menu',
+            'description' => __( 'Display menu to sidebar.', 'four' ),
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</aside>',
+            'before_title'  => '<h2 class="widgettitle">',
+            'after_title'   => '</h2>',
+        )
+
+    );
+    register_sidebar(
+        array(
+            'name' => __( 'Before post contents', 'four' ),
+            'id' => 'before-post-contents',
+            'description' => __( 'Display before post contents.', 'four' ),
+            'before_widget' => '<div class="content">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2>',
+            'after_title'   => '</h2>',
+        )
+    );
+}
+add_action( 'widgets_init', 'theme_slug_widgets_init' );
+
---
+++ index.php
@@ -0,0 +1,212 @@
+<?php // HEADER // ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset=<?php bloginfo('charset'); ?>>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?ver=<?php echo date( 'U' ); ?>" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php // GLOBAL HEADER // ?>
+
+<header class="global header">
+
+    <nav>
+    <?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
+    </nav>
+
+    <?php if ( is_home() || is_front_page() ) : ?>
+    <h1>
+    <?php else : ?>
+    <p>
+    <?php endif; ?>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+        <?php if ( has_custom_logo() ) : ?>
+        <?php the_custom_logo(); ?>
+        <?php else : ?>
+        <?php bloginfo( 'name' ); ?>
+        <?php endif; ?>
+        </a>
+    <?php if ( is_home() || is_front_page() ) : ?>
+    </h1>
+    <?php else : ?>
+    </p>
+    <?php endif; ?>
+
+    <?php if ( is_archive() ) : ?>
+        <h1 class="subtitle"><?php the_archive_title(); ?>の記事一覧</h1>
+    <?php endif; ?>
+    
+    <?php if ( ( is_single() || is_page() ) && !is_front_page() ) : ?>
+        <h1 class="subtitle"><?php if ( mb_strlen( get_the_title() ) === 0 ) : echo '(no title)' ; else : the_title(); endif; ?></h1>
+    <?php endif; ?>
+
+    <?php if ( is_search() ) : ?>
+        <h1 class="subtitle">"<em><?php the_search_query(); ?></em>"の検索結果</h1>
+    <?php endif; ?>
+</header>
+
+<?php // CONTENTS // ?>
+
+<main class="main">
+
+<?php if ( is_home() && is_active_sidebar( 'before-post-contents' ) ) : ?>
+        <article>
+        <?php dynamic_sidebar( 'before-post-contents' ); ?>
+        </article>
+<?php endif; ?>
+
+<?php if ( is_404() ) : ?>
+        <article>
+            <div class="content">
+                <h1 class="title">お探しのページが見つかりません。</h1>
+                <p>そのページは存在しないか、移動した可能性があります。</p>
+                </div>
+        </article>
+<?php elseif ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+        <?php if( $sameDayFlag !== get_post_time( 'Y-m-d', false ) ) : ?>
+<?php
+/**
+ * 投稿タイトルの表示
+ * 投稿ページと固定ページではH1, それ以外ではH2でマークアップ
+ */
+?>
+            <?php if ( is_home() || is_single() || is_archive() || is_search() ) : ?>
+            <header>
+                <ul class="meta">
+                    <li class="post-date">
+                        <time datetime="<?php echo get_post_time( 'Y-m-d\TH:i:s\Z', false ); ?>">
+                        <?php echo get_post_time( 'Y-m-d', false ); ?>
+                        </time>
+                    </li>
+                </ul>
+            </header>
+                <?php endif; ?>
+
+        <?php endif; ?>
+<?php
+/**
+ * 投稿内容の表示
+ * 投稿ページと固定ページで表示
+ */
+?>
+            <?php if ( is_single() || is_page() ) : ?>
+            <div class="content">
+            <?php the_content(); ?>
+            </div> <!-- .content -->
+            <?php wp_link_pages(); ?>
+            <?php elseif ( is_archive() || is_home() || is_search() ) : ?>
+            <div class="content">
+
+            <h2 class="title">
+                <a href="<?php the_permalink(); ?>">
+                    <?php echo mb_substr( get_the_title(), 0, 100 ); ?><?php if ( mb_strlen( get_the_title() ) === 0 ) : echo '(no title)' ; elseif ( mb_strlen( get_the_title() ) >= 100 ) : echo ' ...'; endif; ?>
+                </a>
+            </h2>
+            </div> <!-- .content -->
+            <?php else : ?>
+            <div class="excerpt">
+                    <?php $str_count = mb_strlen( strip_tags( get_the_content() ) ); ?>
+                    <?php echo mb_substr( strip_tags( get_the_content() ), 0, 10 ); ?><?php if ( $str_count === 0 ) : echo '(no content)' ; elseif ( $str_count >= 10 ) : echo ' ...'; endif; ?>
+                    <span class="content-word-count"><?php echo $str_count . 'words.' ?></span>
+                </div>
+            <?php endif; ?>
+<?php
+/**
+ * コメントの投稿とコメントのリストを表示
+ * 投稿ページと固定ページで表示
+ */
+?>
+            <?php if ( ( is_single() || is_page() ) && 1 === 0 ) : ?>
+            <ol class="commentlist">
+            <?php wp_list_comments(); ?>
+            </ol>
+            <?php comments_template(); ?>
+            <?php paginate_comments_links(); ?>
+            <?php endif; ?>
+
+            <?php if ( is_single() || is_page() ) : ?>
+            <footer>
+                <ul class="meta">
+                    <li class="post-date">
+                        <time datetime="<?php echo get_post_time( 'Y-m-d\TH:i:s\Z', false ); ?>">
+                        <?php echo get_post_time( 'Y-m-d H:i:sP', false ); ?>
+                        </time>(GMT: <time datetime="<?php echo get_post_time( 'Y-m-d\TH:i:s\Z', true ); ?>"><?php echo get_post_time( 'Y-m-d H:i:sP', true ); ?></time>)
+                    </li>
+                    <?php if ( get_post_time( 'U' ) !== get_post_modified_time( 'U' ) ) : ?>
+                    <li class="last-modified">
+                        <time datetime="<?php echo get_post_modified_time( 'Y-m-d\TH:i:s\Z', false ); ?>">
+                        <?php echo get_post_modified_time( 'Y-m-d H:i:sP', false ); ?>
+                        </time>(GMT: <time datetime="<?php echo get_post_modified_time( 'Y-m-d\TH:i:s\Z', true ); ?>"><?php echo get_post_modified_time( 'Y-m-d H:i:sP', true ); ?></time>)
+                    </li>
+                    <?php endif; ?>
+                    <?php if ( has_category() ) : ?>
+                    <li class="post-category"><?php the_category( ', ' ); ?></li>
+                    <?php endif; ?>
+                    <?php if ( has_tag() ) : ?>
+                    <li class="post-tag"><?php the_tags( '', ', ', '' ); ?></li>
+                    <?php endif; ?>
+                </ul>
+            </footer>
+
+
+            <?php endif; ?>
+        </article>
+    
+<?php
+/**
+ * アーカイブページで同じ日の投稿を検知するための変数
+ */
+$sameDayFlag = get_post_time( 'Y-m-d', false );
+?>
+
+<?php endwhile; ?>
+<?php else: ?>
+<article>
+<div class="content">
+<p>記事がありません。</p>
+</div>
+</article>
+<?php endif; ?>
+
+    <nav class="paginate_links">
+        <?php echo paginate_links( array(
+            'show_all' => true,
+        ) ); ?>
+    </nav>
+
+</main>
+
+<?php
+/**
+ * サイドバー
+ * 一般的にsidebar.phpに含められる
+ */
+?>
+<?php if ( is_active_sidebar( 'sidebar-menu' ) ) : ?>
+<aside class="sidebar">
+        <?php dynamic_sidebar( 'sidebar-menu' ); ?>
+</aside>
+<?php endif; ?>
+
+
+<?php
+/**
+ * グローバルフッタ
+ * 一般的にfooter.phpに含められる
+ */
+?>
+<footer class="global footer">
+    <nav>
+    <?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+    </nav>
+</footer>
+    
+<?php wp_footer(); ?>
+</body>
+</html>
