5594d0f29564ec76403914c83972c25c8808c770
addshlab <nakad@add.sh>
2020-05-27 21:03:13 +0900

    2020-05-27 21:03:13  M tests/test-page-exists.php

--- tests/test-page-exists.php
+++ tests/test-page-exists.php
@@ -14,7 +14,24 @@ class PageExists extends WP_UnitTestCase {
      * @test
      */
     public function test_is_404() {
-        $this->go_to( home_url() . '?p=10000000' );
+        $this->go_to( home_url() . '?p=99999999' );
         $this->assertTrue( is_404() );
     }
+
+    public function test_is_404() {
+        $this->go_to( home_url() . '?p=99999999' );
+        $this->assertTrue( is_404() );
+    }
+
+    public function test_post_content() {
+        global $post;
+        $post_id = $this->factory->post->create( array( 'post_content' => 'test content' ) );
+        $post = get_post( $post_id );
+        setup_postdata( $post );
+
+        $content2 = 'test content' . PHP_EOL;
+        $this->expectOutputString( $content2 );
+        the_content();
+}
+
 }
