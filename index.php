d593a2bbeeb36649b15716111e668ae1f3aadac9
addshlab <nakad@add.sh>
2020-06-15 15:38:34 +0900

    2020-06-15 15:38:34  M index.php

--- index.php
+++ index.php
@@ -188,7 +188,7 @@ $sameDayFlag = get_post_time( 'Y-m-d', false );
  * 一般的にsidebar.phpに含められる
  */
 ?>
-<?php if ( ! is_front_page() && is_active_sidebar( 'sidebar-menu' ) ) : ?>
+<?php if ( ( get_option( 'show_on_front' ) === 'page' && ! is_front_page() ) && is_active_sidebar( 'sidebar-menu' ) ) : ?>
 <aside class="sidebar">
         <?php dynamic_sidebar( 'sidebar-menu' ); ?>
 </aside>
